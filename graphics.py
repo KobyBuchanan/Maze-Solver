b5aca787a9305274d44117dc7b654c36174be7cb
KobyBuchanan <kobya.buchanan@gmail.com>
2024-08-29 14:44:21 -0400

    maze solver implemented

--- graphics.py
+++ graphics.py
@@ -44,16 +44,16 @@ class Line:
 
 
 class Cell:
-    def __init__(self, win):
+    def __init__(self, win=None):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
+        self.visited = False
         self._x1 = None
         self._x2 = None
         self._y1 = None
         self._y2 = None
-        self.visited = False
         self._win = win
 
     def draw(self, x1, y1, x2, y2):
